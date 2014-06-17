653b5cdbda1d358f62ec411bb098a58dbd1e5ed1
oskar13 <oskar92@tlu.ee>
2014-06-17 16:33:00 +0300

    Muudatused

---
+++ themes/twentyfourteen-child/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
+		
+		<link href="http://fonts.googleapis.com/css?family=Economica:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<div id="wrapper">
+			<div id="container">
+			<!--Header - this can be included everywhere!-->
+			<header>
+				<a href="index.php"><img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" /></a>
+				<span>
+					<a href="#">EST</a>
+					<a href="#">ENG</a>
+				</span>
+			</header>
+			<!--Navigation bar (animated with CSS3)-->
+			<nav>
+				<a href="index.php">Avaleht</a>
+				<a href="uudised.php">Uudised</a>
+				<a href="fookus.php">Fookus</a>
+				<a href="arhiiv.php">Arhiiv</a>
+				<a href="meeskond.php">Meeskond</a>
+				<a href="toetajad.php">Toetajad</a>
+				<a href="galerii.php">Galerii</a>
+				<a href="kontakt.php">Kontakt</a>
+			</nav>	
+
+
+
---
+++ themes/twentyfourteen-child/single-videouudis.php
@@ -0,0 +1,34 @@
+
+			<?php get_header(); ?>	
+
+
+			<hr style="background-color:#11A9A7">
+			<div id="news_border">
+				<div id="news_news">
+					<img id="news_vid" src="">
+				</div>
+			</div>
+				<div id="news_clips" class="clips">
+					<p>Viimati lisatud</p>
+					<span>Video 1</span>
+					<span>Video 2</span>
+					<span>Video ...</span>
+					<a href="arhiiv.php"><img class="arrow" src="images/arrow.png"></a>
+				</div>
+				<div id="news_clips" class="clips">
+					<p>Populaarsed</p>
+					<span>Video 1</span>
+					<span>Video 2</span>
+					<span>Video ...</span>
+					<a href="arhiiv.php"><img class="arrow" src="images/arrow.png"></a>
+				</div>
+				<div id="news_text" class="clips">
+					<p>Tekstuudised</p>
+					<span>Pilt 1</span>
+					<span>Pilt 2</span>
+					<span>Pilt ...</span>
+					<a href="arhiiv.php"><img class="arrow" src="images/arrow.png"></a>
+				</div>	
+		</div>	
+
+		<?php get_footer(); ?>	
